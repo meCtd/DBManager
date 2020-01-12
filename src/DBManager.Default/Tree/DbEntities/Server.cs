c63d81f109901d3f2fad0c91aa6966b12362cb94
MrYandersen <vladislav.lugansky@gmail.com>
2020-01-12 15:17:30 +0200

    Added Server DbEntity

---
+++ src/DBManager.Default/Tree/DbEntities/Server.cs
@@ -0,0 +1,11 @@
+﻿namespace DBManager.Default.Tree.DbEntities
+{
+	public class Server : DbObject
+	{
+		public override MetadataType Type => MetadataType.Server;
+
+		public Server(string name)
+			: base(name)
+		{	}
+	}
+}
--- src/DBManager.Default/Tree/MetadataType.cs
+++ src/DBManager.Default/Tree/MetadataType.cs
@@ -6,18 +6,19 @@ namespace DBManager.Default.Tree
 	public enum MetadataType
 	{
 		None = 0,
-		Database = 1 << 1,
-		Schema = 1 << 2,
-		Table = 1 << 3,
-		View = 1 << 4,
-		Function = 1 << 5,
-		Procedure = 1 << 6,
-		Constraint = 1 << 7,
-		Column = 1 << 8,
-		Trigger = 1 << 9,
-		Parameter = 1 << 10,
-		Key = 1 << 11 ,
-		Index = 1 << 12,
-		Type = 1 << 13
+		Server = 1 << 1,
+		Database = 1 << 2,
+		Schema = 1 << 3,
+		Table = 1 << 4,
+		View = 1 << 5,
+		Function = 1 << 6,
+		Procedure = 1 << 7,
+		Constraint = 1 << 8,
+		Column = 1 << 9,
+		Trigger = 1 << 10,
+		Parameter = 1 << 11,
+		Key = 1 << 12,
+		Index = 1 << 13,
+		Type = 1 << 14,
 	}
 }
